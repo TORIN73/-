cfe2d5c41db34f16b5eaee27110364589d6bc4dd
TORIN73 <tortorizmarvel@gmail.com>
2026-05-23 11:18:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE_NAME = 'goalpath-v1';
+const CACHE_NAME = 'goalpath-v2';
 const ASSETS = [
-  './index.html',
-  './manifest.json'
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => {}))
   );
   self.skipWaiting();
 });
